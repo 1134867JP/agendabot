3a1d40e1c5dd4c146208790d0570534790ecfd8c
João Pedro Rodrigues <131798086+1134867JP@users.noreply.github.com>
2026-07-24 10:23:19 -0300

    Complete security hardening: resources/views/app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -18,7 +18,7 @@
         <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,300;0,9..40,400;0,9..40,500;0,9..40,600;1,9..40,400&family=Instrument+Serif:ital@0;1&family=Syne:wght@400;600;700;800&display=swap" rel="stylesheet">
 
         <!-- Scripts -->
-        @routes
+        @routes(nonce: Vite::cspNonce())
         @viteReactRefresh
         @vite(['resources/js/app.tsx', "resources/js/Pages/{$page['component']}.tsx"])
         @inertiaHead
