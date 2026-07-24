d334e56e78d4adbfbc240f3193b2e3fcec82f240
João Pedro Rodrigues <131798086+1134867JP@users.noreply.github.com>
2026-07-24 10:23:47 -0300

    Complete security hardening: database/seeders/DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,23 +2,13 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        // Super Admin
-        User::firstOrCreate(
-            ['email' => 'joao@agendou.com'],
-            [
-                'name'           => 'João Pedro',
-                'password'       => Hash::make('password'),
-                'is_super_admin' => true,
-            ]
-        );
+        $this->call(SuperAdminSeeder::class);
 
         // Tenants reais
         $this->call(OdontoExcellenceSeeder::class);
